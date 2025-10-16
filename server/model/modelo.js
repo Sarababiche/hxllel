b6293d7dec5ef5187fdf02b3567826091f7e0d64
Sarababiche <charly231004@gmail.com>
2025-10-15 22:04:47 -0600

    cambie dialectoptions

--- server/model/modelo.js
+++ server/model/modelo.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   dialect: 'mysql',
-  dialectOptions: isProduction,
+dialectOptions: isProduction ? { ssl: { require: true } } : {}
   host: isProduction ? undefined : process.env.DB_HOST,
   port: isProduction ? undefined : parseInt(process.env.DB_PORT || '3306'),
   logging: false,
